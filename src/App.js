64eaff21e786af8667867f1e274aac411481fae9
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2018-12-27 22:54:23 +0000

    react redux first tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import ButtonOne from './react-hoc/components/buttonOne.js'
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-     </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+			<div> Age: <span>{this.props.age}</span> </div>
+			<button onClick={this.props.onAgeUp}>Age Up</button>
+			<button onClick={this.props.onAgeDown}>Age Down</button>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		age: state.age
+	};
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onAgeUp: () => dispatch({type:'AGE_UP'}),
+		onAgeDown: () => dispatch({type:'AGE_DOWN'}),
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
