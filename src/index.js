c02ec91a37a4e32ada99a4c4186a2c315c56fe56
Ganesh Ranganathan <ganesh.ranganathan.85@gmail.com>
2018-12-28 17:18:45 +0000

    Latest create-react-app2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reducer from './store/reducer';
+import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import  { watchAgeUp } from './sagas/saga';
-import createSagaMiddleware from 'redux-saga'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(watchAgeUp);
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
